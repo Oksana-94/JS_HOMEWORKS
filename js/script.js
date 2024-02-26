f20d2a9d4dcab4da96d05998b0e78b02e4491122
Oksana <oksanabondar.0511@gmail.com>
2024-02-26 14:24:23 +0200

    hw-7

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+// Task 1
+const name = 'John';
+const admin = name;
+alert(admin);
+
+
+// Task 2
+const planet = 'Earth';
+const user = 'John';
+
+
+// Task 3
+const conc = '10' + 10;
+console.log(typeof(conc));
+console.log(conc);
