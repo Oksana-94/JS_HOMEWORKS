d542873d54228ccf4143e089600db42df8c84c31
Oksana <oksanabondar.0511@gmail.com>
2024-02-27 10:41:43 +0200

    hw-6

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
 // Task 1
-let str1 = prompt();
-let str2 = prompt();
-let str3 = prompt();
-
-console.log(str1 + ' ' + str2 + ' ' + str3);
+// let str1 = prompt();
+// let str2 = prompt();
+// let str3 = prompt();
+//
+// console.log(str1 + ' ' + str2 + ' ' + str3);
 
 //Task 2
+const num = 62739;
+const str = num + '';
+console.log(typeof str);
+console.log(str);
+console.log(str.split('').join(' '));
