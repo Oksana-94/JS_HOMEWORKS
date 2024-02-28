56b82ad0671bcacc699b9f3e6eebcd0b7e5e512e
Oksana <oksanabondar.0511@gmail.com>
2024-02-28 19:08:09 +0200

    hw-8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let myAge = prompt('Введіть Ваш вік');
-let age = +myAge + 10;
+const myAge = prompt('Введіть Ваш вік');
+const age = +myAge + 10;
 alert('Через 10 років Вам виповниться ' + age + ' років.');
