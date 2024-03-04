c60e8abe49201698fb2c146696e1a3eb2208416e
Oksana <oksanabondar.0511@gmail.com>
2024-03-04 18:05:02 +0200

    hw-9

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const num1 = +prompt('Введіть перше число');
+const num2 = +prompt('Введіть друге число');
+
+const sum = num1 + num2;
+console.log('Результат: ' + num1 + ' + ' + num2 + ' = ' + sum);
+const min = num1 - num2;
+console.log('Результат: ' + num1 + ' - ' + num2 + ' = ' + min);
+const mult = num1 * num2;
+console.log('Результат: ' + num1 + ' * ' + num2 + ' = ' + mult);
+const divide = num1 / num2;
+console.log('Результат: ' + num1 + ' / ' + num2 + ' = ' + divide);
