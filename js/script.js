e353a933646c5623b0d67833cabeba372110064b
Oksana <oksanabondar.0511@gmail.com>
2024-03-05 14:00:52 +0200

    hw-16 fixed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let num1 = +prompt('Введіть перше число');
-let num2 = +prompt('Введіть друге число');
-let num3 = +prompt('Введіть третє число');
+const num1 = +prompt('Введіть перше число');
+const num2 = +prompt('Введіть друге число');
+const num3 = +prompt('Введіть третє число');
 
 alert(`Середнє арифметичне значення вказаних чисел: ${(num1 + num2 + num3) / 3}`);
