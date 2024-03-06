d24e9ff171744bb0e19d449a0c1cf695c7bfb49f
Oksana <oksanabondar.0511@gmail.com>
2024-03-06 13:59:04 +0200

    hw-14

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const num1 = +prompt('Введіть перше число');
+const num2 = +prompt('Введіть друге число');
+
+alert(`${num1} + ${num2} = ${num1 + num2}
+${num1} - ${num2} = ${num1 - num2}
+${num1} * ${num2} = ${num1 * num2}
+${num1} / ${num2} = ${num1 / num2}`);
