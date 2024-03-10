2fb79adee833bc12e59ef8c2d6e1305302d7a53e
Oksana <oksanabondar.0511@gmail.com>
2024-03-10 15:12:09 +0200

    hw-15

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const action = prompt('Вкажіть, яку дію Ви хочете виконати, де add - додавання, sub - віднімання, mult - множення, div - ділення');
+const num1 = +prompt('Вкажіть перше число');
+const num2 = +prompt('Вкажіть друге число');
+
+
+if(action === 'add') {
+    alert(`${num1} + ${num2} = ${num1 + num2}`);
+} else if(action === 'sub') {
+    alert(`${num1} - ${num2} = ${num1 - num2}`);
+} else if(action === 'mult') {
+    alert(`${num1} * ${num2} = ${num1 * num2}`);
+} else if(action === 'div') {
+    alert(`${num1} / ${num2} = ${num1 / num2}`);
+} else {
+    alert('Невірно вказана дія');
+}
