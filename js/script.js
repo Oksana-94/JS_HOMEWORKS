72dc3e78fa240f935b4bc5c11ec11756129a5c26
Oksana <oksanabondar.0511@gmail.com>
2024-03-13 16:59:55 +0200

    hw-24

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+// 1
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+let square = 0;
+
+for (let i of arr) {
+    sum += i;
+    square += Math.pow(i, 2);
+
+}
+console.log(sum);
+console.log(square);
+
