b21ad51931380a64948292e55cf66cda93e90b5f
Oksana <oksanabondar.0511@gmail.com>
2024-03-14 11:21:29 +0200

    hw-25

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const num = +prompt('Введіть число');
+let i = 2;
+for (; i <= 1000000; i++) {
+    if (num % i === 0) break;
+}
+
+switch(true) {
+    case num === 1:
+        console.log('Ви ввели цифру 1. Відповідно, дільник також 1.');
+        break;
+    case num < 1:
+        console.log('NaN');
+        break;
+    case num >= 1:
+        console.log(i);
+        break;
+}
