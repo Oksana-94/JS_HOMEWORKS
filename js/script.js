4bb6d6d58931af211ee499aff5dfddc6c3f2b4c1
Oksana <oksanabondar.0511@gmail.com>
2024-03-18 19:13:11 +0200

    hw-29

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const str = 'Hello';
+const num = 6;
+const sym = '*';
+const boo = true;
+function padString(string, number, symbol, boolean) {
+
+    if(string === undefined) return 'Помилка! Ви не ввели перший аргумент';
+    if(number === undefined) return 'Помилка! Ви не ввели другий аргумент!';
+    if(symbol === undefined) return 'Помилка! Ви не ввели третій аргумет!';
+    if(boolean === undefined) return 'Помилка! Ви не ввели четвертий аргумент!';
+
+    while(string.length < number && boolean === true) {
+        string += symbol;
+    }
+
+    while(string.length < number && boolean === false) {
+        string = symbol + string;
+    }
+
+    return string.substring(0, number);
+}
+
+console.log(padString(str, num, sym, boo));
