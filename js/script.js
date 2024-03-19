15e421fdff19db0f4dd2cb9105faa6570502cedc
Oksana <oksanabondar.0511@gmail.com>
2024-03-19 12:28:39 +0200

    hw-30

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const arr = [1, 2, 3, -1, -2, -3, -4];
+// const arr = [];
+const checkArr = function (array) {
+
+    let exampleArr = [];
+
+    if (array.length === 0) return 'масив, переданий як параметр у функцію, є порожнім';
+
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] < 0) continue;
+        exampleArr.push(array[i]);
+    }
+    if (exampleArr.length === 0) return null;
+    return exampleArr;
+
+}
+
+console.log(checkArr(arr));
