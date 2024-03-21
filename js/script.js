866b5e30c670b547581298cef104feb77faf3572
Oksana <oksanabondar.0511@gmail.com>
2024-03-21 22:22:30 +0200

    hw-28

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const num1 = +prompt('Введіть число');
 const num2 = +prompt('Введіть ступінь, до якого це число необхідно піднести');
 
-function toExponentiation(number, degree = 1) {
+const toExponentiation = function (number, degree = 1) {
 
     if (isNaN(number) || isNaN(degree)) return 'Помилка';
 
