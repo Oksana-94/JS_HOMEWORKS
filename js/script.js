85760ca99f22cf42d43bfd206dc8d0c4e6a41563
Oksana <oksanabondar.0511@gmail.com>
2024-03-21 22:18:04 +0200

    hw-30

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const arr = [1, 2, 3, -1, -2, -3, -4];
+const arr = [1, 2, 3, -1, -2, -3];
 // const arr = [];
 const checkArr = function (array) {
 
@@ -12,9 +12,8 @@ const checkArr = function (array) {
         if (array[i] < 0) continue;
         exampleArr.push(array[i]);
     }
-    if (exampleArr.length === 0) return null;
-    return exampleArr;
 
+    return (exampleArr.length === 0) ? null : exampleArr;
 }
 
 console.log(checkArr(arr));
