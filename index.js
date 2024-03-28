4ca3c1239f2de25b7aec7e9b14f22ac58dafa720
Oksana <oksanabondar.0511@gmail.com>
2024-03-28 13:43:18 +0200

    hw-33

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const sub = function(a, b) {
+    return a - b;
+}
+
+const mul = function(a, b) {
+    return a * b;
+}
+
+console.log(add(4, 3));
+console.log(sub(4, 3));
+console.log(mul(4, 3));
